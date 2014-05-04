204849363bbd03335ce608bce329561645311e0a
rohit <nrohits@gmail.com>
2014-05-04 01:40:38 -0700

    moving webapp into python api server

--- pythonapiserver/saferoute/saferoute/settings.py
+++ pythonapiserver/saferoute/saferoute/settings.py
@@ -72,6 +72,17 @@ DATABASES = {
     }
 }
 
+TEMPLATE_DIRS = (
+    BASE_DIR + '/templates/',
+)
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
@@ -88,8 +99,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    BASE_DIR + '/static',
+)
 
 
 # My settings
